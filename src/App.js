65489e7d7ab677878c645af13b3ca100d7871ad6
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-25 19:46:53 -0700

    added images and changed header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from "./logo.svg";
+import React from "react";
+import { Container } from "react-bootstrap";
+import Navigation from "./Navigation";
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Resume from "./Resume";
+
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <Container>
+      <Navigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/resume" elements={<Resume />}></Route>
+        </Routes>
+      </main>
+    </Container>
   );
 }
 
