8234a28e6bcd19abdaa2b4c3e578e52a5ab05a50
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-26 01:04:49 -0700

    loaded resume jpeg but not rendering

--- src/App.js
+++ src/App.js
@@ -17,8 +17,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
-          <Route path="/projects" element={<Projects />}></Route>
-          <Route path="/resume" elements={<Resume />}></Route>
         </Routes>
       </main>
     </Container>
--- src/Resume.js
+++ src/Resume.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Image, Container } from "react-bootstrap";
+import Ori_Ofir_Single_Page_Resume_Web_Dev from "./Images/Ori_Ofir_Single_Page_Resume_Web_Dev.jpg";
 
 function Resume(props) {
-  return <div></div>;
+  return (
+    <Container>
+      <h1>TEST</h1>
+      <Image src={Ori_Ofir_Single_Page_Resume_Web_Dev} />
+    </Container>
+  );
 }
 
 export default Resume;
