fad7ed8a0c752dcd4b8bae6ed462f812ece9998a
oriofir <97814056+oriofir@users.noreply.github.com>
2022-12-08 16:46:13 -0800

    resume download button made

--- src/Resume.js
+++ src/Resume.js
@@ -5,19 +5,21 @@ import "./Resume.css";
 import Ori_Ofir_Single_Page_Resume_Web_Dev from "./Images/Ori_Ofir_Single_Page_Resume_Web_Dev.jpg";
 
 function Resume(props) {
-  const handleClick = () => {
-    window.open(
-      "https://docs.google.com/document/d/1Pdg6seUzxzDU1tsypwpi6qjEE9rJEnIxzpkH9cAz1ZI/edit"
-    );
-  };
+  // const handleClick = () => {
+  //   window.open(
+  //     "https://docs.google.com/document/d/1Pdg6seUzxzDU1tsypwpi6qjEE9rJEnIxzpkH9cAz1ZI/edit"
+  //   );
+  // };
   return (
     <Container className="resume-container">
       <div className="resume-title">RESUME</div>
+
+      <button className="res-download">DOWNLOAD RESUME</button>
+
       <Image
         className="resume"
-        style={{ maxWidth: 1000, cursor: "pointer" }}
+        style={{ maxWidth: 1000 }}
         src={Ori_Ofir_Single_Page_Resume_Web_Dev}
-        onClick={handleClick}
       />
     </Container>
   );
