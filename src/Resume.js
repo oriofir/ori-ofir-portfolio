80fe6f90a45d450a3a237557f562f4fa3becf1bd
oriofir <97814056+oriofir@users.noreply.github.com>
2022-12-29 11:21:57 -0800

    deleted commented out code

--- src/Resume.js
+++ src/Resume.js
@@ -5,12 +5,6 @@ import "./Resume.css";
 import Ori_Ofir_Single_Page_Resume_Web_Dev from "./Images/___ Ori Ofir Single Page Resume Web Dev (1)_Page_1.jpg";
 
 function Resume(props) {
-  // const handleClick = () => {
-  //   window.open(
-  //     "https://docs.google.com/document/d/1Pdg6seUzxzDU1tsypwpi6qjEE9rJEnIxzpkH9cAz1ZI/edit"
-  //   );
-  // };
-
   const downloadPDF = () => {
     fetch("___ Ori Ofir Single Page Resume Web Dev.pdf").then((response) => {
       response.blob().then((blob) => {
