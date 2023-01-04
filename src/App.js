cf38f73fa09e0205901bf29d5f8e41d61c5e83a3
oriofir <97814056+oriofir@users.noreply.github.com>
2023-01-03 16:49:54 -0800

    added a sticky navbar that follows with scroll

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from "react";
 // import { Container } from "react-bootstrap";
-import Navigation from "./Navigation";
+// import Navigation from "./Navigation";
 import Projects from "./Projects";
 import About from "./About";
 // import Projects from "./Projects";
 import Resume from "./Resume";
-
+import { Link } from "react-scroll";
 import "./App.css";
 import TechStack from "./TechStack";
 import "animate.css";
-import { Routes, Route } from "react-router-dom";
+
+// import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<About />}></Route>
-        <Route path="/techstack" element={<TechStack />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/resume" element={<Resume />}></Route>
-      </Routes>
+      <header className="nav">
+        <nav className="nav__container__actions">
+          <ul>
+            <li>
+              <Link activeClass="active" smooth spy to="about">
+                ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="techstack">
+                TECH STACK
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="projects">
+                PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="resume">
+                RESUME
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section id="about">{<About />}</section>
+      <section id="techstack">{<TechStack />}</section>
+      <section id="projects">{<Projects />}</section>
+      <section id="resume">{<Resume />}</section>
     </>
   );
 }
