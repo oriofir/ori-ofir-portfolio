cc50ba546d2238688af6a775e4c1e846b3fc4a75
oriofir <97814056+oriofir@users.noreply.github.com>
2023-07-17 16:33:28 -0700

    started from scratch

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
-import React from "react";
-// import { Container } from "react-bootstrap";
-// import Navigation from "./Navigation";
-import Projects from "./Projects";
-import About from "./About";
-// import Projects from "./Projects";
-import Resume from "./Resume";
-import { Link } from "react-scroll";
-import "./App.css";
-import TechStack from "./TechStack";
-import "animate.css";
-
-// import { Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <header className="nav">
-        <nav className="nav__container__actions">
-          <ul>
-            <li>
-              <Link activeClass="active" smooth spy to="about">
-                ABOUT
-              </Link>
-            </li>
-            <li>
-              <Link activeClass="active" smooth spy to="techstack">
-                TECH STACK
-              </Link>
-            </li>
-            <li>
-              <Link activeClass="active" smooth spy to="projects">
-                PROJECTS
-              </Link>
-            </li>
-            <li>
-              <Link activeClass="active" smooth spy to="resume">
-                RESUME
-              </Link>
-            </li>
-          </ul>
-        </nav>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <section id="about">{<About />}</section>
-      <section id="techstack">{<TechStack />}</section>
-      <section id="projects">{<Projects />}</section>
-      <section id="resume">{<Resume />}</section>
-    </>
+    </div>
   );
 }
 
